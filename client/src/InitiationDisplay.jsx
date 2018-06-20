7f6469cf197b35fd2648c312bbbd6d2093295e0e
Scott Wilde <scottpaulwilde@gmail.com>
2018-06-19 21:17:48 -0700

    change location display

--- client/src/InitiationDisplay.jsx
+++ client/src/InitiationDisplay.jsx
@@ -12,18 +12,23 @@ export class InitiationDisplay extends React.Component {
         let actualDate = formatDate(o.actualDate);
 
         let personLink = this.props.showPerson ? <div className="field person"><PersonLink person={o.person} /></div> : "";
+
+        // sponsors
         let sponsor1 = <div className="field person"><PersonLink person={o.sponsor1_person} altNameFirst={o.sponsor1First} altNameLast={o.sponsor1Last} /></div>;
         let sponsor2 = <div className="field person"><PersonLink person={o.sponsor2_person} altNameFirst={o.sponsor2First} altNameLast={o.sponsor2Last} /></div>;
-
         if (this.props.dontShowIf) {
             if (o.sponsor1_person && this.props.dontShowIf.personId === o.sponsor1_person.personId) sponsor1 = "";
             if (o.sponsor2_person && this.props.dontShowIf.personId === o.sponsor2_person.personId) sponsor2 = "";
         }
 
+        // location
+        let locationUrl = 'index.html?locationid=' + o.locationId;
+        let location = <div className="field locationName"><a href={locationUrl}>{o.location}</a></div>;
+
         return <div className="initiation">
             {personLink}
             <div className="field degree">{degree.name}</div>
-            <div className="field locationName">{o.location}</div>
+            {location}
             <div className="field actualDate">{actualDate}</div>
             {sponsor1}
             {sponsor2}
