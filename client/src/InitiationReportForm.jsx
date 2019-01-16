eb39baf3c4b3493cb15dde60fbdc593139623d26
Scott Wilde <scottpaulwilde@gmail.com>
2019-01-15 20:33:02 -0800

    location and initiation pickers work on initiation report page

--- client/src/InitiationReportForm.jsx
+++ client/src/InitiationReportForm.jsx
@@ -20,25 +20,15 @@ export class InitiationReportForm extends React.Component {
         super(props);
 
         this.officerPickers = [];
-        this.candidatePickers = [];
+        this.candidatePickers = [React.createRef()];
 
         this.state = {
             errors:[],
             message: "",
 
             degreeId: getDegreeByName('1').degreeId,
-            locationId: null,
 
-            candidates:[
-                {
-                    personId: null,
-                    person: null,
-                    notes: ""
-                }
-            ],
-
-            //candidatePersonIds: [null],
-            //candidateNotes: [""]
+            candidateCount: 1
         };
 
     }
@@ -54,80 +44,6 @@ export class InitiationReportForm extends React.Component {
         });
     }
 
-    handleIndexedChange (event) {
-        const target = event.target;
-        let value = (target.hasOwnProperty('type') && target.type === 'checkbox') ? target.checked : target.value;
-        if (target.type === 'radio') value = value === 'true';
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleLocationChange (event) {
-        const target = event.target;
-        const name = target.name;
-        const nameNew = target.nameNew;
-        const id = target.location.locationId;
-
-        if (id === -1) {
-            this.setState({
-                [name]: id,
-                [nameNew]: target.location.data
-            });
-        }
-        else {
-            this.setState({
-                [name]: id,
-                [nameNew]: null
-            });
-        }
-
-    }
-    handleIndexedPersonChange (event) {
-        const target = event.target;
-        const name = target.name;
-        const nameNew = target.nameNew;
-        const id = target.person.personId;
-        const index = target.index;
-
-        let o = this.state.candidates[index];
-
-
-        if (id === -1) {
-            o.personId = id;
-            o.person = target.person.data;
-            this.setState({ candidates : this.state.candidates });
-        }
-        else {
-            o.personId = id;
-            o.person = null;
-            this.setState({ candidates : this.state.candidates });
-        }
-
-    }
-
-    handlePersonChange (event) {
-        const target = event.target;
-        const name = target.name;
-        const nameNew = target.nameNew;
-        const id = target.personId;
-
-        if (id === -1) {
-            this.setState({
-                [name]: id,
-                [nameNew]: target.person.data
-            });
-        }
-        else {
-            this.setState({
-                [name]: id,
-                [nameNew]: null
-            });
-        }
-
-    }
     handleDegreeChange(event) {
         this.setState({degreeId: +event.target.value});
     }
@@ -163,16 +79,8 @@ export class InitiationReportForm extends React.Component {
     }
 
     addCandidate() {
-
-        let candidates = this.state.candidates.slice(0);
-
-        candidates.push({
-            personId: null,
-            person: null,
-            notes: ""
-        });
-
-        this.setState({ candidates:candidates });
+        this.candidatePickers.push(React.createRef());
+        this.setState({ candidateCount: this.state.candidateCount + 1});
     }
 
     render() {
@@ -188,47 +96,55 @@ export class InitiationReportForm extends React.Component {
             return <option value={degree.degreeId} key={i}>{degree.name}</option>;
         });
 
+        // manage officers
         let officers = getOfficerByDegreeId(this.state.degreeId);
+
+        // fix the pickers
+        let target = officers.map(o => { return o.officerId.toString(); });
+        let existing = Object.keys(this.officerPickers);
+        // remove
+        existing.forEach(key => {
+            if (target.indexOf(key) === -1) delete this.officerPickers[key];
+        });
+        // add
+        target.forEach(key => {
+            if (existing.indexOf(key) === -1) this.officerPickers[key] = React.createRef();
+        });
+
+        // create the officer rows
         let officerInput = officers.map((officer, i) => {
-            this.officerPickers[i] = this.officerPickers[i] || React.createRef();
-            let name = officer.name.toLowerCase() + "_personId";
-            let nameNew = officer.name.toLowerCase() + "_person";
+            let key = officer.officerId.toString();
+
             return <div className="formLine" key={i}>
                 <div className="formItem">
                     <div className="formItemTitle">{officer.name}</div>
-                    <PersonPicker ref={this.officerPickers[i]} name={name} />
+                    <PersonPicker ref={this.officerPickers[key]} name={"officer" + key} index={i} />
                 </div>
             </div>;
         });
 
-        let candidates = this.state.candidates.map((c, i) => {
-
-            let name = "candidate" + i + "_personId";
-            //let nameNew = "candidate" + i + "_person";
-            //let notesName = "candidate" + i + "_notes";
-            //let notes = this.state.candidateNotes[i];
-            this.candidatePickers[i] = this.candidatePickers[i] || React.createRef();
-            return <div className="formLine" key={i}>
+        // create the candidate rows
+        let candidates = [];
+        for (let i = 0; i < this.state.candidateCount; i++) {
+            let o = <div className="formLine" key={i}>
                 <div className="formItem">
                     <div className="formItemTitle">Candidate {i+1}</div>
-                    <PersonPicker name={name} ref={this.candidatePickers[i]} />
-                </div>
-                <div className="formItem">
-                    <div className="formItemTitle">Notes</div>
-                    <div><textarea type="text" cols="25" rows="1" name="notes" index={i} value={c.notes} onChange={this.handleIndexedChange.bind(this)} /></div>
+                    <PersonPicker ref={this.candidatePickers[i]} name={"candidate" + i.toString()} index={i} />
                 </div>
             </div>;
-        });
+
+            candidates.push(o);
+        }
 
         return <div>
 
             <div className="formItem">
                 <div className="formItemTitle">L/O/C performed initiation</div>
-                <div><LocationPicker name="performedAt_locationId" nameNew="performedAt_location" onChange={this.handleLocationChange.bind(this)} /></div>
+                <div><LocationPicker name="performedAt_locationId" /></div>
             </div>
             <div className="formItem">
                 <div className="formItemTitle">Submitted through Lodge / Oasis</div>
-                <div><LocationPicker name="submittedThrough_locationId" nameNew="submittedThrough_location" onChange={this.handleLocationChange.bind(this)} /></div>
+                <div><LocationPicker name="submittedThrough_locationId" /></div>
             </div>
 
             <div className="formLine indent" style={{marginTop:'1em'}}>
