1d150669d02da0e9b8cac86cf7127aeebe021bbb
Scott Wilde <scottpaulwilde@gmail.com>
2019-03-23 15:12:55 -0700

    annual report

--- server/data2/degree.js
+++ server/data2/degree.js
@@ -10,22 +10,22 @@ let fields = [
 
 
 exports.values = [
-    {name:'Minerval', rank:1, degreeId:1},
-    {name:'First', rank:2, degreeId:2},
-    {name:'Second', rank:3, degreeId:3},
-    {name:'Third', rank:4, degreeId:4},
-    {name:'Fourth', rank:5, degreeId:5},
-    {name:'PI', rank:6, degreeId:6},
-    {name:'KEW', rank:7, degreeId:7},
-    {name:'Fifth', rank:8, degreeId:8},
-    {name:'KRE', rank:9, degreeId:9},
-    {name:'Sixth', rank:10, degreeId:10},
-    {name:'GIC', rank:11, degreeId:11},
-    {name:'PRS', rank:12, degreeId:12},
-    {name:'Seventh', rank:13, degreeId:13},
-    {name:'Eighth', rank:14, degreeId:14},
-    {name:'Ninth', rank:15, degreeId:15},
-    {name:'Tenth', rank:16, degreeId:16}
+    {name:'Minerval', shortName:'0', rank:1, degreeId:1},
+    {name:'First', shortName:'1', rank:2, degreeId:2},
+    {name:'Second', shortName:'2', rank:3, degreeId:3},
+    {name:'Third', shortName:'3', rank:4, degreeId:4},
+    {name:'Fourth', shortName:'4', rank:5, degreeId:5},
+    {name:'PI', shortName:'PI', rank:6, degreeId:6},
+    {name:'KEW', shortName:'KEW', rank:7, degreeId:7},
+    {name:'Fifth', shortName:'5', rank:8, degreeId:8},
+    {name:'KRE', shortName:'KRE', rank:9, degreeId:9},
+    {name:'Sixth', shortName:'6', rank:10, degreeId:10},
+    {name:'GIC', shortName:'GIC', rank:11, degreeId:11},
+    {name:'PRS', shortName:'PRS', rank:12, degreeId:12},
+    {name:'Seventh', shortName:'7', rank:13, degreeId:13},
+    {name:'Eighth', shortName:'8', rank:14, degreeId:14},
+    {name:'Ninth', shortName:'9', rank:15, degreeId:15},
+    {name:'Tenth', shortName:'10', rank:16, degreeId:16}
 ];
 
 exports.lookup = {};
---
+++ server/reports/yearly.js
@@ -0,0 +1,158 @@
+let Initiation = require('../data2/initiation');
+let Location = require('../data2/location');
+let Degree = require('../data2/degree');
+let Database = require('../data2/database');
+
+
+(async () => {
+
+    let year = 2016;
+
+    let raw = {
+        initiations: null,
+        locations: null
+    };
+
+    Database.init(Database.storageType.file);
+
+    // load the data
+    await Promise.all([
+        Initiation.selectAll().then(res => {raw.initiations = res;}), Location.selectAll().then(res => {raw.locations = res;})
+    ]);
+
+    // split into locations
+    let allLocations = {};
+
+    allLocations[-1] = {
+        initiations: [],
+        apps: [],
+        added: false,
+        data: {
+            name: 'n/a',
+            type: ''
+        }
+
+    };
+
+    raw.locations.forEach(l => {
+        allLocations[l.locationId] = l;
+        l.initiations = [];
+        l.apps = [];
+        l.added = false;
+    });
+
+    // filter to just last years inits that have an actual date
+    let min = new Date(`3/1/${year}`);
+    let max = new Date(`3/1/${year+1}`);
+    let initiations = raw.initiations.filter(initiation => {
+        if (initiation.data.actualDate === null) return false;
+        let d = new Date(initiation.data.actualDate);
+        return d >= min && d < max;
+    });
+
+    let apps = raw.initiations.filter(initiation => {
+        if (initiation.data.localBodyDate !== null || initiation.data.signedDate !== null || initiation.data.approvedDate !== null) {
+            let d = new Date(initiation.data.approvedDate || initiation.data.localBodyDate || initiation.data.signedDate);
+            return d >= min && d < max;
+        }
+
+        // has a proposed date but no actual date
+        if (initiation.data.proposedDate !== null && initiation.data.actualDate === null) {
+            let d = new Date(initiation.data.proposedDate);
+            return d >= min && d < max;
+        }
+
+        // otherwise default to the actual date
+        if (initiation.data.actualDate !== null) {
+            let d = new Date(initiation.data.actualDate);
+            return d >= min && d < max;
+        }
+
+        return false;
+    });
+
+    // give them to the locations
+    let locations = [];
+    initiations.forEach(initiation => {
+        if (initiation.data.performedAt_locationId === null) {
+            console.log('unknown');
+            initiation.data.performedAt_locationId = -1;
+            //return false;
+        }
+
+        let location = allLocations[initiation.data.performedAt_locationId];
+        location.initiations.push(initiation);
+
+        // filter to just locations that have inits
+        if (!location.added) {
+            locations.push(location);
+            location.added = true;
+        }
+
+    });
+
+    apps.forEach(initiation => {
+        if (initiation.data.performedAt_locationId === null) {
+            console.log('unknown');
+            initiation.data.performedAt_locationId = -1;
+        }
+
+        let location = allLocations[initiation.data.performedAt_locationId];
+        location.apps.push(initiation);
+
+        // filter to just locations that have inits
+        if (!location.added) {
+            locations.push(location);
+            location.added = true;
+        }
+
+    });
+
+    // for each of these, separate the inits by degree
+    let lines = [];
+
+    // the headers
+    let headers = ['Location', 'Type', '0 APP'];
+    Degree.values.forEach(degree => {
+        headers.push(degree.shortName);
+    });
+    lines.push(headers.join('\t'));
+
+    //locations.sort((a, b))
+    locations.forEach(location => {
+
+        // sort inits by degreeid
+        let count = {};
+        let appCount = {};
+        location.initiations.forEach(initiation => {
+            let degreeId = initiation.data.degreeId;
+            if (!count.hasOwnProperty(degreeId)) count[degreeId] = 0;
+            count[degreeId] += 1;
+        });
+        location.apps.forEach(initiation => {
+            let degreeId = initiation.data.degreeId;
+            if (!appCount.hasOwnProperty(degreeId)) appCount[degreeId] = 0;
+            appCount[degreeId] += 1;
+        });
+
+        // write the line out
+        let line = [location.data.name, location.data.type];
+
+        // minerval app
+        if (appCount.hasOwnProperty(1)) line.push(appCount[1].toString());
+        else line.push(0);
+
+        // then each init
+        Degree.values.forEach(degree => {
+            if (count.hasOwnProperty(degree.degreeId)) line.push(count[degree.degreeId].toString());
+            else line.push(0);
+        });
+
+        lines.push(line.join('\t'));
+    });
+
+
+    const fs = require('fs');
+    fs.writeFileSync(`annual${year}.txt`, lines.join('\n'));
+
+})();
