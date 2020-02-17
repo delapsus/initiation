7249c11872456029c1457c7c32d38d70829fc2fe
Scott Wilde <scottpaulwilde@gmail.com>
2020-02-17 14:57:14 -0800

    stop audit during install

--- run-withBuild.command
+++ run-withBuild.command
@@ -2,9 +2,9 @@
 cd -- "$(dirname "$BASH_SOURCE")"
 git pull https://otoinitiation:h4bixKTYeWh3@github.com/asicath/initiation.git
 cd server
-npm install
+npm install --loglevel=error --no-audit
 cd ../client
-npm install
+npm install --loglevel=error --no-audit
 npm run build
 cd ..
 node ./server/app.js
--- windows_run-withBuild.cmd
+++ windows_run-withBuild.cmd
@@ -1,10 +1,10 @@
 git pull https://otoinitiation:h4bixKTYeWh3@github.com/asicath/initiation.git
 git reset --hard
 cd server
-call npm install --loglevel=error
+call npm install --loglevel=error --no-audit
 cd ..
 cd client
-call npm install --loglevel=error
+call npm install --loglevel=error --no-audit
 call npm run build
 cd ..
 node ./server/app.js
