627bc5468ddf66c56256ade1735bf4b85c98429c
Scott Wilde <scottpaulwilde@gmail.com>
2020-02-17 14:53:31 -0800

    errors only

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,10 +26,9 @@ var config = {
                 use: [ 'style-loader', 'css-loader' ]
             }
         ]
+
     },
-    stats: {
-        warnings: false
-    }
+    stats: 'errors-only'
 };
 
 module.exports = config;
